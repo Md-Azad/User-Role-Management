f3efefaf07901d08ae910253383e5b49df2b65da
Md-Azad <sokalazad@gmail.com>
2024-01-07 17:53:39 +0100

    implemented post delete functionality

--- src/layout/Dashboard.jsx
+++ src/layout/Dashboard.jsx
@@ -1,15 +1,11 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 import { AuthContext } from "../Providers/AuthProvider";
-import axios from "axios";
 
 const Dashboard = () => {
   const [accessOpen, setAccessOpen] = useState(false);
   const [contentOpen, setContentOpen] = useState(false);
-
-  const [accessPermission, setAccessPermission] = useState("");
-  const [userPosition, setUserPosition] = useState("");
-  const { user } = useContext(AuthContext);
+  const { accessPermission, userPosition } = useContext(AuthContext);
 
   const toggleAccess = () => {
     setAccessOpen(!accessOpen);
@@ -18,27 +14,6 @@ const Dashboard = () => {
   const toggleContent = () => {
     setContentOpen(!contentOpen);
   };
-
-  useEffect(() => {
-    const email = user.email;
-    axios
-      .get(`http://localhost:5000/users/${email}`, {})
-      .then((res) => {
-        const permit = res.data.permission?.Permission;
-
-        const position = res.data.position;
-        if (position) {
-          setUserPosition(position);
-        } else {
-          console.log("normal user");
-        }
-
-        if (permit) {
-          setAccessPermission(permit);
-        }
-      })
-      .catch((err) => console.error(err));
-  }, [user.email]);
 
   return (
     <div className="drawer lg:drawer-open">
@@ -68,6 +43,43 @@ const Dashboard = () => {
           className="drawer-overlay"
         ></label>
         <ul className="menu p-4 w-60 min-h-full bg-base-200 text-base-content">
+          {userPosition ? (
+            <>
+              <li>
+                <Link to="/dashboard">Dashboard</Link>
+              </li>
+              <li>
+                <a onClick={toggleAccess}>Access</a>
+                {accessOpen && (
+                  <ul>
+                    <li>
+                      {" "}
+                      <Link to="/dashboard/usermanage">
+                        User Management
+                      </Link>{" "}
+                    </li>
+
+                    <li>
+                      <Link to="/dashboard/rolemanage">Role Management</Link>
+                    </li>
+                  </ul>
+                )}
+              </li>
+              <li>
+                <a onClick={toggleContent}>Content management</a>
+                {contentOpen && (
+                  <ul>
+                    <li>
+                      <Link to="/dashboard/postmanage">Post Manage</Link>
+                    </li>
+                  </ul>
+                )}
+              </li>
+            </>
+          ) : (
+            <></>
+          )}
+
           {accessPermission.includes("View User") ? (
             <>
               <li>
@@ -97,7 +109,6 @@ const Dashboard = () => {
                     ) : (
                       <></>
                     )}
-                    {/* <li> <Link to="/dashboard/rolemanage">Role Management</Link> </li> */}
                   </ul>
                 )}
               </li>
@@ -105,24 +116,6 @@ const Dashboard = () => {
           ) : (
             <></>
           )}
-
-          {/* <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-
-          <li>
-            <a onClick={toggleAccess}>Access</a>
-            {accessOpen && (
-              <ul>
-              
-                <li> <Link to="/dashboard/usermanage">User Management</Link> </li>
-
-               
-                <li> <Link to="/dashboard/rolemanage">Role Management</Link> </li>
-              </ul>
-            )}
-          </li> */}
-
           {accessPermission.includes("View Post") ? (
             <>
               {" "}
@@ -140,16 +133,7 @@ const Dashboard = () => {
           ) : (
             <></>
           )}
-          {/* <li>
-            <a onClick={toggleContent}>Content management</a>
-            {
-              contentOpen && (
-                <ul>
-                  <li><Link to="/dashboard/postmanage">Post Manage</Link></li>
-                </ul>
-              )
-            }
-          </li> */}
+
           <li>
             <Link to="/dashboard/manage"> Manage</Link>
           </li>
